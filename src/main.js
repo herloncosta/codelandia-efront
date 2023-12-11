f17445257ae468dffe29df4ad7ad7a4ee92c7e53
herloncosta <herlon36@gmail.com>
2023-12-10 21:08:30 -0300

    add .prettierrc

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 export function createUser(name, age, height) {
-  console.log("testando plugin husky");
-  return {
-    name,
-    age,
-    height,
-  };
+    console.log('testando plugin husky')
+    return {
+        name,
+        age,
+        height,
+    }
 }
 
-createUser("Herlon", 27, 1.8);
-createUser("Herlon", 27, 1.8);
+createUser('Herlon', 27, 1.8)
+createUser('Herlon', 27, 1.8)
