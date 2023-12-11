82f75088a2f30821b852f83630f922dce2cd71b1
herloncosta <herlon36@gmail.com>
2023-12-10 21:21:01 -0300

    add header mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#90A1FF",
-      },
+    content: ['./src/index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#90A1FF',
+                secondary: '#130B1D',
+                dark: '#08040D',
+                darken: '#030007',
+            },
+        },
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+}
